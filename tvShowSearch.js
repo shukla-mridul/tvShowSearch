fd9edc82377553ff94c52f7f3a4d92dbd7ae1137
mridulshukla01 <shukla.mridul.ms@gmail.com>
2024-06-30 17:30:55 +0530

    optimization and enhancing

--- tvShowSearch.js
+++ tvShowSearch.js
@@ -5,6 +5,40 @@ const minRating=document.querySelector('#minrating');
 const searchBtn=document.querySelector('#searchbtn');
 const displaySection=document.querySelector('#display');
 
+const preDisplayShows=(shows,rating=10)=>{
+    for(let show of shows){
+       
+        try{
+        // console.log(show);
+        // console.log(show.name);
+        // console.log(show.image.medium);
+        let container=document.createElement('div');
+        let img=document.createElement('img');
+        let title=document.createElement('p');
+        img.setAttribute('src',show.image.medium);
+        title.innerText=show.name + `  -rating : ${show.rating.average}`;
+        container.append(img);
+        img.after(title);
+        displaySection.append(container);
+        // // console.log(show.show.name,show.show.rating.average);
+        }catch (e){
+            console.log(e,"unavkaibae");
+        }
+
+        
+
+    }
+}
+
+const renderShows=async()=>{
+    const showsMetaData =await axios.get('https://api.tvmaze.com/shows');
+    let showDetails=showsMetaData.data.splice(0,60);
+    console.log(showDetails);
+    preDisplayShows(showDetails);
+}
+// preemptively hsowing shows!
+renderShows();
+
 const getFormData=()=>{
     const showDetails={
         name: `${showName.value}`,
@@ -56,6 +90,7 @@ showSearch.addEventListener('submit',async(evt)=>{
     //     console.log(airQurl);
     // }
     let shows=await axios.get(baseUrl,config);
+    // console.log(shows.data);
     displayShows(shows.data,showDetails.minimumRating);
     // showName.value='';
 })
